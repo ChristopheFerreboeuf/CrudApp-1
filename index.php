a4bd518e84cbf905bee36decf737ab55c0ec9789
Alvis <alvisgrigaluns@gmail.com>
2021-12-21 13:59:18 +0200

    product

--- index.php
+++ index.php
@@ -5,13 +5,10 @@ require_once('entity/Product.php');
 include 'includes/init.php';
 include 'includes/header.php';
 
-/*$typeData = new TypeRepository();
-$types = $typeData->getTypes();*/
-
 $productData = new ProductRepository();
 $products = $productData->getProducts();
 
-/*$product_id = $products->getProduct('');
+/*$product_id = $products->getProduct();
 var_dump($product_id);*/
 ?>
 
@@ -34,13 +31,13 @@ var_dump($product_id);*/
                             <input class="form-check-input delete-checkbox" type="checkbox" value="<?= $product->getId() ?>">
                         </div>
                         <div class="card-body text-center">
-                            <h4 class="card-title">SKU: <?= $product->getSku() ?> </h4>
-                            <h5 class="card-title">Name: <?= $product->getName() ?> </h5>
-                            <p class="card-text">Price: <?= $product->getPrice() ?> &euro;</p>
-                            <p class="card-text">Type: <?= $product->getType() ?> </p>
-                            <p class="card-text">Size: <?= $product->getSize() ?> </p>
-                            <p class="card-text">Weight: <?= $product->getWeight() ?> </p>
-                            <p class="card-text">Length: <?= $product->getLength() ?> </p>
+                            <h4 class="card-title"> <?= $product->getSku() ?> </h4>
+                            <h5 class="card-title"> <?= $product->getName() ?> </h5>
+                            <p class="card-text"> <?= $product->getPrice() ?> &euro;</p>
+                            <p class="card-text"> <?= $product->getType() ?> </p>
+                            <p class="card-text"> <?= $product->getSize() ?> MB</p>
+                            <p class="card-text"> <?= $product->getWeight() ?> KG</p>
+                            <p class="card-text">Dimension: <?= $product->getLength() ?> </p>
                         </div>
                     </div>
                 </div>
